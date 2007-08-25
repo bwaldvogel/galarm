4243ca2c45a2fb80bd83de53607b5e7c06a1d32f
Benedikt Waldvogel <galarm at 0x11.net>
2007-08-25 04:35:45 +0200

    • libnotify popup instead of own • glib regular expressions used to parse time format (much more powerful now)

--- src/global.h
+++ src/global.h
@@ -8,8 +8,9 @@
 #include <sys/stat.h>    /* umask */
 #include <errno.h>
 #include <time.h>        /* localtime, time, gmtime */
-#include <gtk/gtk.h>     /* 2.10 required */
+#include <fcntl.h>       /* creat - g_creat needs it! (?) */
+#include <gtk/gtk.h>     /* 2.14 required */
 #include <glib.h>
 #include <glib/gstdio.h> /* g_creat */
-#include <fcntl.h>       /* creat - g_creat needs it! (?) */
+#include <libnotify/notify.h>
 #include "galarm_config.h"
--- src/main.c
+++ src/main.c
@@ -1,33 +1,36 @@
 /*
- * written by Benedikt Waldvogel, bene@0x11.net
- * October/November 2006
+ * written by Benedikt Waldvogel, bene /at/ 0x11.net
+ * Oct 2006 - Aug 2007
  *
- * Requires GTK+ 2.10 because of GtkStatusIcon
+ * Requires GTK+ 2.14 because of Perl-compatible regular expressions
+ * -- Requires GTK+ 2.10 because of GtkStatusIcon
  */
 #include "global.h"
 
-/* globals {{{ */
-static const guint MAX_SECONDS = 24 * 3600;
-static const guint BLINK_TRESHOLD = 10;
+/* globals */
+static const guint          MAX_SECONDS       = 7 *24 *3600;
+static const guint          BLINK_TRESHOLD    = 10;
 
-static gboolean daemonize = FALSE;
-static gboolean quiet = FALSE;
-static gboolean verbose = FALSE;
-static gboolean stopall = FALSE;
-static gchar **opt_remaining = NULL;
+static gboolean             daemonize         = FALSE;
+static gboolean             quiet             = FALSE;
+static gboolean             verbose           = FALSE;
+static gboolean             stopall           = FALSE;
+static gchar              **opt_remaining     = NULL;
 
 /* flag which indicates whether we are in
  * countdown mode or in fixed-endtime mode */
-gboolean countdownMode = TRUE;
-static gdouble secondsToCount = 0.0l; /* countdown mode */
-static time_t endtime = 0;            /* fixed-endtime mode */
+gboolean                    countdownMode     = TRUE;
+static gdouble              secondsToCount    = 0.0l;        /* countdown mode     */
+static time_t               endtime           = 0;           /* fixed-endtime mode */
 
-static gboolean timer_paused = FALSE;
-static GtkStatusIcon *icon;
-static GTimer *timer;
-static gchar *alarm_message = NULL;
+static gboolean             timer_paused      = FALSE;
+static GtkStatusIcon       *icon;
+static GTimer              *timer;
+static gchar               *alarm_message     = NULL;
+static gchar                DEFAULT_MESSAGE[] = "alarm";
 
-static GError *error = NULL;
+static GError              *error             = NULL;
+static NotifyNotification  *notification      = NULL;
 
 static GOptionEntry entries[] = {
 	{"daemon", 'd', 0, G_OPTION_ARG_NONE, &daemonize, "start as daemon", NULL},
@@ -37,9 +40,8 @@ static GOptionEntry entries[] = {
 	{"stop-all", 0, G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &stopall, NULL, NULL},
 	{G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, &opt_remaining, "Message to display", "TIMEOUT [MESSAGE]"}
 };
-/* }}} */
 
-static time_t now(void) /* {{{ */
+static time_t now(void)
 {
 	time_t now = time(NULL);
 	if (now == ((time_t)-1)) {
@@ -48,93 +50,175 @@ static time_t now(void) /* {{{ */
 	}
 	return now;
 }
-/* }}} */
 
-static void set_endtime(const char* time_str) /* {{{ */
+static void set_endtime(const char* time_str)
 {
 	time_t n = now();
-	struct tm *temp_end = localtime(&n);
 
-	gchar **splits = g_strsplit(time_str, ":", 3);
+	/* RELATIVE TIME FORMATS
+		1.5d   → 36 hours
+		1,2m   → 1 minute and 12 seconds
+		1h     → 3600 seconds
+		1:12m  → the same
+		9.5s   → 9 seconds since precision is only one second
+		1:30d  → 1 and a half day
 
-	/* HOUR */
-	if (splits[0] != NULL) {
-		unsigned long hour = strtoul(splits[0], NULL, 10);
-		if (hour >= 24 || splits[0][0] == 0) {
-			g_printerr("invalid hour: %s\n", g_strerror(ERANGE));
-			exit(EXIT_FAILURE);
-		}
-		temp_end->tm_hour = hour;
-
-		/* MINUTE */
-		if (splits[1] != NULL) {
-			unsigned long min = strtoul(splits[1], NULL, 10);
-			if (min >= 60 || splits[1][0] == 0) {
-				g_printerr("invalid minute: %s\n", g_strerror(ERANGE));
-				exit(EXIT_FAILURE);
-			}
-			temp_end->tm_min = min;
-			/* SECONDS */
-			if (splits[2] != NULL) {
-				unsigned long sec = strtoul(splits[2], NULL, 10);
-				if (sec >= 60 || splits[2][0] == 0) {
-					g_printerr("invalid seconds: %s\n", g_strerror(ERANGE));
-					exit(EXIT_FAILURE);
-				}
-				temp_end->tm_sec = sec;
-			} else {
-				/* case no seconds provided (@17:23 eg) */
-				temp_end->tm_sec = 0;
-			}
-		} else {
-			/* case no minutes and seconds provided (@18 eg) */
-			temp_end->tm_min = 0;
-			temp_end->tm_sec = 0;
-		}
-
-	} else {
-		g_printerr("invalid time\n");
+	   ABSOLUTE TIME FORMATS
+		@12:30 → at half past 12pm
+		@9pm   → 21 o'clock
+	*/
+	GRegex *reltime = g_regex_new("^(?P<First>\\d{1,5})(?P<Part2>(?P<Format>[:,.])(?P<Second>\\d{1,6}))?(?P<Qualifier>[dhms]?)$",
+			G_REGEX_CASELESS | G_REGEX_OPTIMIZE, 0, &error);
+	if (reltime == NULL) {
+		g_printerr("g_regex_new: %s\n", error->message);
+		error = NULL;
 		exit(EXIT_FAILURE);
 	}
-	g_strfreev(splits);
-	endtime = mktime(temp_end);
-	/* if hour is before now, assume the next day is meant */
-	if (endtime <= n)
-		endtime += 24 * 3600;
-}
-/* }}} */
+	GRegex *abstime = g_regex_new("^\\@(?P<Hour>\\d{1,2})(:(?P<Minute>[0-5]?[0-9]))?(:(?P<Second>[0-5]?[0-9]))?(?P<AmPm>[ap]m)?$",
+			G_REGEX_CASELESS | G_REGEX_OPTIMIZE, 0, &error);
+	if (abstime == NULL) {
+		g_printerr("g_regex_new: %s\n", error->message);
+		error = NULL;
+		exit(EXIT_FAILURE);
+	}
 
-static gint quit(gpointer data) /* {{{ */
+	GMatchInfo *matchinfo;
+
+	if (g_regex_match(reltime, time_str, 0, &matchinfo)) {
+		gchar  *first      = g_match_info_fetch_named (matchinfo, "First");
+		gchar  *part2      = g_match_info_fetch_named (matchinfo, "Part2");
+		gchar  *format     = g_match_info_fetch_named (matchinfo, "Format");
+		gchar  *second     = g_match_info_fetch_named (matchinfo, "Second");
+		gchar  *qualifier  = g_match_info_fetch_named (matchinfo, "Qualifier");
+		glong   first_num  = atol(first);
+		// days, hours, minutes and seconds
+		// default is minutes
+		switch (qualifier[0]) {
+			case 'd':
+				secondsToCount = 24*3600;
+				break;
+			case 'h':
+				secondsToCount = 3600;
+				break;
+			case 's':
+				secondsToCount = 1;
+				break;
+			case 'm':
+			case '\0':
+			default:
+				secondsToCount = 60;
+				break;
+		}
+
+		if (part2[0]) {
+			long double f,v;
+			glong   second_num = atol(second);
+			switch (format[0]) {
+				case ':':
+					if (second_num >= 60) {
+						g_printerr("provide a valid timeout. see --help\n");
+						exit(EXIT_FAILURE);
+					}
+					f = first_num;
+					f += (long double)second_num/60.0;
+					secondsToCount *= f;
+					break;
+				case '.':
+				case ',':
+					f = first_num;
+					// 923823 → 0.923823
+					v = second_num;
+					while (v>1) v/=(long double)10;
+					f += v;
+					secondsToCount *= f;
+
+					break;
+				default:
+					// this mustn't happen since we catch the format in the regex
+					// and this switch case 'should' be closely tied to the pattern
+					g_assert(FALSE);
+			}
+		} else {
+			secondsToCount *= first_num;
+		}
+		g_free(first), g_free(part2), g_free(format), g_free(second), g_free(qualifier);
+
+	} else if (g_regex_match(abstime, time_str, 0, &matchinfo)) {
+
+		countdownMode = FALSE;
+
+		gchar *hour    = g_match_info_fetch_named (matchinfo, "Hour");
+		gchar *minute  = g_match_info_fetch_named (matchinfo, "Minute");
+		gchar *seconds = g_match_info_fetch_named (matchinfo, "Second");
+		gchar *ampm    = g_match_info_fetch_named (matchinfo, "AmPm");
+		glong  h       = atol(hour);
+
+		if (h>=24) {
+			g_printerr("provide a valid timeout. see --help\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if (ampm && g_strncasecmp(ampm, "pm", 2) == 0)
+			h+=12;
+
+		struct tm *end = localtime(&n);
+		end->tm_hour = h;
+
+		if (minute)
+			end->tm_min = atol(minute);
+		else
+			end->tm_min = 0;
+
+		if (seconds)
+			end->tm_sec = atol(seconds);
+		else
+			end->tm_sec = 0;
+
+		g_free(hour), g_free(minute), g_free(seconds), g_free(ampm);
+		/* if hour is before now, assume the next day is meant */
+		endtime = mktime(end);
+
+		if (endtime <= n)
+			endtime += 24 * 3600;
+
+	} else {
+		g_printerr("provide a valid timeout. see --help\n");
+		exit(EXIT_FAILURE);
+	}
+
+	g_regex_unref(reltime);
+	g_regex_unref(abstime);
+}
+
+static gint quit(gpointer data)
 {
+	if (!notify_notification_close(notification, &error)) {
+		g_printerr("notify_notification_close: %s\n", error->message);
+		error = NULL;
+	}
 	gtk_main_quit();
 	return FALSE;
 }
-/* }}} */
 
-static gint play_sound(gpointer data) /* {{{ */
-{				
-	if (!quiet)
-	{
-		if (sound_cmd == NULL || g_utf8_collate(sound_cmd, "") == 0) {
-			g_printerr("please provide a sound_cmd in the config file.\n");
-		} else {
-
-			char *argv[] = { sound_cmd, "alert.wav", NULL };
-			g_spawn_async(DATADIR "/sounds/galarm", argv, NULL,
-					G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, &error);
-			if (error != NULL) {
-				g_printerr("g_spawn_async: %s\n", error->message);
-				exit(EXIT_FAILURE);
-			}
-		}
+static gint play_sound(gpointer data)
+{
+	if (sound_cmd == NULL || g_utf8_collate(sound_cmd, "") == 0) {
+		g_printerr("please provide a sound_cmd in the config file.\n");
 	} else {
-		g_debug("quiet mode - not playing sounds");
+
+		char *argv[] = { sound_cmd, "alert.wav", NULL };
+		g_spawn_async(DATADIR "/sounds/galarm", argv, NULL,
+				G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, &error);
+		if (error != NULL) {
+			g_printerr("g_spawn_async: %s\n", error->message);
+			error = NULL;
+			exit(EXIT_FAILURE);
+		}
 	}
 	return FALSE;		/* don't continue */
 }
-/* }}} */
 
-static gint pause_resume(gpointer data) /* {{{ */
+static gint pause_resume(gpointer data)
 {
 	if (timer_paused) {
 		g_timer_continue(timer);
@@ -145,69 +229,43 @@ static gint pause_resume(gpointer data) /* {{{ */
 
 	return FALSE;		/* don't continue */
 }
-/* }}} */
 
-static gint show_alarm(gpointer data) /* {{{ */
+static void prepare_notification (void)
 {
-	/* window */
-	GtkWidget *alarm_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_decorated(GTK_WINDOW(alarm_window), FALSE);
-	/* hints: keep above, sticky and don't show in pager */
-	gtk_window_set_keep_above(GTK_WINDOW(alarm_window), TRUE);
-	gtk_window_stick(GTK_WINDOW(alarm_window));
-	gtk_window_set_skip_pager_hint(GTK_WINDOW(alarm_window), TRUE);
-	gtk_window_set_accept_focus(GTK_WINDOW(alarm_window), FALSE);
-
-	/* label */
-	GtkWidget *header_label = gtk_label_new(NULL);
-	gchar *text = g_markup_printf_escaped("<big><b>Alarm</b></big>");
-	gtk_label_set_markup(GTK_LABEL(header_label), text);
-	g_free(text);
-	gtk_label_set_line_wrap(GTK_LABEL(header_label), TRUE);
-
-	GtkWidget *vbox = gtk_vbox_new(FALSE, 5);
-	GtkWidget *hbox = gtk_hbox_new(FALSE, 5);
-
-	/* xalign, yalign, xscale, yscale */
-	GtkWidget *align = gtk_alignment_new(0.0f, 0.0f, 1.0f, 1.0f);
-	gtk_alignment_set_padding(GTK_ALIGNMENT(align), 10, 10, 10, 10);
-	GtkWidget *eventbox = gtk_event_box_new();
-	g_signal_connect(GTK_OBJECT(eventbox), "button_press_event",
-			G_CALLBACK(quit), NULL);
-	GtkWidget *frame = gtk_frame_new(NULL);
-
-	GtkWidget *image =
-		gtk_image_new_from_file(DATADIR "/pixmaps/galarm.png");
-	g_assert(image != NULL);
-
-	gtk_box_pack_start(GTK_BOX(vbox), header_label, TRUE, TRUE, 0);
-
-	if (alarm_message != NULL) {
-		GtkWidget *message_label = gtk_label_new(alarm_message);
-		gtk_box_pack_end(GTK_BOX(vbox), message_label, TRUE, TRUE, 0);
+	if (!notify_init("galarm")) {
+		g_printerr("notify_init failed");
+		exit(EXIT_FAILURE);
 	}
 
-	gtk_box_pack_start(GTK_BOX(hbox), image, TRUE, TRUE, 0);
-	gtk_box_pack_end(GTK_BOX(hbox), vbox, TRUE, TRUE, 0);
+	notification = notify_notification_new ("Alarm", alarm_message, NULL, NULL);
+	g_signal_connect (notification, "closed", G_CALLBACK(quit), NULL);
+	GdkPixbuf *icon = gdk_pixbuf_new_from_file(DATADIR "/pixmaps/galarm.png", &error);
+	if (icon == NULL) {
+		g_printerr("gdk_pixbuf_new_from_file: %s\n", error->message);
+		error = NULL;
+	} else {
+		notify_notification_set_icon_from_pixbuf(notification, icon);
+	}
+	notify_notification_set_urgency (notification, NOTIFY_URGENCY_CRITICAL);
+	notify_notification_set_timeout(notification, 5000);
+}
 
-	gtk_container_add(GTK_CONTAINER(align), hbox);
-	gtk_container_add(GTK_CONTAINER(frame), align);
-	gtk_container_add(GTK_CONTAINER(eventbox), frame);
-	gtk_container_add(GTK_CONTAINER(alarm_window), eventbox);
-
-	gtk_widget_show_all(alarm_window);
-	gtk_idle_add(play_sound, NULL);
+static gint show_alarm(gpointer data)
+{
+	if (!notify_notification_show (notification, &error)) {
+		g_printerr("notify_notification_show: %s\n", error->message);
+		error = NULL;
+	}
+	if (!quiet)
+		gtk_idle_add(play_sound, NULL);
 
 	return FALSE;
 }
-/* }}} */
 
-static void statusicon_activate(GtkStatusIcon * icon, gpointer data) /* {{{ */
-{
-}
-/* }}} */
+static void statusicon_activate(GtkStatusIcon * icon, gpointer data)
+{ }
 
-static void statusicon_popup(GtkStatusIcon * status_icon, guint button, guint activate_time, gpointer user_data) /* {{{ */
+static void statusicon_popup(GtkStatusIcon * status_icon, guint button, guint activate_time, gpointer user_data)
 {
 	/*** CREATE A MENU ***/
 	GtkWidget *menu = gtk_menu_new();
@@ -234,9 +292,9 @@ static void statusicon_popup(GtkStatusIcon * status_icon, guint button, guint ac
 			gtk_status_icon_position_menu,
 			icon, button, activate_time);
 }
-/* }}} */
 
-static gint galarm_timer(gpointer data) /* {{{ */
+/* this function is called periodically */
+static gint galarm_timer(gpointer data)
 {
 	time_t diff_time = -1; /* default: invalid value */
 
@@ -260,7 +318,7 @@ static gint galarm_timer(gpointer data) /* {{{ */
 	}
 
 	GString *buffer = g_string_sized_new(50);
-	g_string_printf(buffer, "%s: ", (alarm_message != NULL) ? alarm_message : "alarm");
+	g_string_printf(buffer, "%s: ", alarm_message);
 
 	char timeBuffer[1024];
 	if (strftime(timeBuffer, sizeof(timeBuffer), "%H:%M:%S", gmtime(&diff_time)) == 0) {
@@ -291,9 +349,8 @@ static gint galarm_timer(gpointer data) /* {{{ */
 
 	return FALSE;		/* don't continue */
 }
-/* }}} */
 
-int main(int argc, char **argv) /* {{{ */
+int main(int argc, char **argv)
 {
 	timer = g_timer_new();
 	g_assert(timer != NULL);
@@ -305,61 +362,33 @@ int main(int argc, char **argv) /* {{{ */
 
 	context = g_option_context_new(NULL);
 	g_assert(context != NULL);
-	g_option_context_set_summary(context,
-			"Possible Timeout values:\n  5s\t\t5 seconds\n  6.5m\t\t6:30 minutes\n  19\t\t19 minutes\n  7h\t\t7 hours\n  @17\t\tat 17:00 o'clock\n  @17:25\tat 17:25 o'clock");
+	g_option_context_set_help_enabled(context, TRUE);
+	/* g_option_context_set_summary(context,
+			"Possible Timeout values:\n  5s\t\t5 seconds\n  6.5m\t\t6:30 minutes\n  19\t\t19 minutes\n  7h\t\t7 hours\n  @17\t\tat 17:00 o'clock\n  @17:25\tat 17:25 o'clock"); */
 	g_option_context_add_main_entries(context, entries, NULL);
+
 	/* adds GTK+ options */
 	g_option_context_add_group(context, gtk_get_option_group(TRUE));
-	if (g_option_context_parse(context, &argc, &argv, &error) == FALSE) {
+	if (g_option_context_parse(context, &argc, &argv, NULL) == FALSE) {
 		g_printerr("%s\n", error->message);
+		error = NULL;
 		exit(EXIT_FAILURE);
 	}
+
 	g_option_context_free(context);
 
 	if (opt_remaining == NULL || opt_remaining[0] == NULL) {
-		g_printerr("provide a timeout. see --help\n");
+		g_printerr("provide a valid timeout. see --help\n");
 		exit(EXIT_FAILURE);
 	}
-	gchar *last;
 
-	if (opt_remaining[0][0] == '@')
-	{
-		countdownMode = FALSE;
-		if (g_utf8_strlen(opt_remaining[0], -1) < 2) {
-			g_printerr("Please provide a valid endtime. see --help\n");
-			exit(EXIT_FAILURE);
-		}
-		set_endtime(&opt_remaining[0][1]);
-	} else {
-		countdownMode = TRUE;
-		secondsToCount = g_ascii_strtod(opt_remaining[0], &last);
-		if (secondsToCount == HUGE_VAL || secondsToCount == -HUGE_VAL) {
-			g_printerr("strtod failed: %s\n", g_strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-
-		switch (*last) {
-			case 'h':
-				secondsToCount *= 60.0l;
-			case 'm':
-				secondsToCount *= 60.0l;
-			case 's':
-				break;
-			default:
-				/* default case is 'm' */
-				secondsToCount *= 60.0l;
-		}
-	}
-
-	if (secondsToCount < 0.0l || secondsToCount > MAX_SECONDS) {
-		g_printerr("Please provide a valid timeout (0..%d seconds)\n",
-				MAX_SECONDS);
-		exit(EXIT_FAILURE);
-	}
+	set_endtime(opt_remaining[0]);
 
 	/* the first argument is the timeout value */
 	if (opt_remaining[1] != NULL)
 		alarm_message = g_strjoinv(" ", opt_remaining + 1);
+	if (alarm_message == NULL || alarm_message[0]==0)
+		alarm_message = DEFAULT_MESSAGE;
 
 	if (daemonize) {
 		if (fork() != 0) {
@@ -388,11 +417,12 @@ int main(int argc, char **argv) /* {{{ */
 			G_CALLBACK(statusicon_popup), NULL);
 	gtk_status_icon_set_visible(icon, TRUE);
 
-	galarm_timer(0);
+	prepare_notification();
+	// start the timer loop
+	galarm_timer(NULL);
 	gtk_main();
 
 	return 0;
 }
-/* }}} */
 
-/* vim: set foldmethod=marker foldlevel=0 : */
+// vim: set foldmethod=syntax foldlevel=0 :
